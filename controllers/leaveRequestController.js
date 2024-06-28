b951e8bea157dd3c050c288fd3be5eb0f2427af2
Zohaib24648 <z.mughal.24648@khi.iba.edu.pk>
2024-06-28 05:45:49 +0500

    finalized

--- controllers/leaveRequestController.js
+++ controllers/leaveRequestController.js
@@ -1,6 +1,8 @@
 // controllers/leaveRequestController.js
 
 const LeaveRequest = require('../models/LeaveRequest');
+const Attendance = require('../models/Attendance');
+
 
 // Get leave requests for a user
 exports.getLeaveRequests = async (req, res) => {
@@ -49,13 +51,31 @@ exports.updateLeaveRequestStatus = async (req, res) => {
 
   try {
     const leaveRequest = await LeaveRequest.findById(id);
-    
+
     if (!leaveRequest) {
       return res.status(404).json({ message: 'Leave request not found' });
     }
 
     leaveRequest.status = status;
     await leaveRequest.save();
+
+    // If the leave request is approved, update the attendance records
+    if (status === 'approved') {
+      const { userId, startDate, endDate } = leaveRequest;
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+      let currentDate = start;
+
+      while (currentDate <= end) {
+        await Attendance.findOneAndUpdate(
+          { userId: userId, date: currentDate },
+          { status: 'leave' },
+          { upsert: true, new: true }
+        );
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+    }
+
     res.json(leaveRequest);
   } catch (err) {
     res.status(500).json({ message: 'Server Error', error: err.message });
